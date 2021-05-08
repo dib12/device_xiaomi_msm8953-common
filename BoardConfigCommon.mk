5c5d417cdce95a8fa830e3ab25b2c94ee6c879f6
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2021-05-08 16:23:22 +0200

    msm8953-common: Force inclusion of BoardConfigVendor.mk
    Change-Id: Id299b1917dd95b606a392d443f743a7abf5b70e8

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -206,4 +206,4 @@ WIFI_HIDL_FEATURE_DISABLE_AP_MAC_RANDOMIZATION := true
 WPA_SUPPLICANT_VERSION := VER_0_8_X
 
 # Inherit from the proprietary version
--include vendor/xiaomi/msm8953-common/BoardConfigVendor.mk
+include vendor/xiaomi/msm8953-common/BoardConfigVendor.mk
