1986df7fe98855a7b72e1986ed9b863757d41873
dib12 <61652361+dib12@users.noreply.github.com>
2023-04-18 20:37:06 +0530

    Common: Built GrapheneCamera by default

--- msm8953.mk
+++ msm8953.mk
@@ -124,7 +124,7 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.4-impl \
     android.hardware.camera.provider@2.4-service \
-    Snap
+    GrapheneCamera
 
 # Display
 PRODUCT_PACKAGES += \
---
+++ prebuilt/Android.mk
@@ -0,0 +1,12 @@
+LOCAL_PATH := $(call my-dir)
+
+include $(CLEAR_VARS)
+
+LOCAL_MODULE := GrapheneCamera
+LOCAL_MODULE_CLASS := APPS
+LOCAL_PRODUCT_MODULE := true
+LOCAL_CERTIFICATE := PRESIGNED
+LOCAL_SRC_FILES := GrapheneCamera.apk
+LOCAL_OVERRIDES_PACKAGES := Aperture Camera2 Snap
+
+include $(BUILD_PREBUILT)
