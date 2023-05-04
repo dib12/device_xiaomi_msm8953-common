180f3291d03d77eefd33cc9fdc626f9bb0e3e54b
dib12 <61652361+dib12@users.noreply.github.com>
2023-05-04 11:55:24 +0530

    clean tissot

--- liblight/Android.mk
+++ liblight/Android.mk
@@ -17,18 +17,15 @@ LOCAL_PATH:= $(call my-dir)
 # hw/<COPYPIX_HARDWARE_MODULE_ID>.<ro.board.platform>.so
 include $(CLEAR_VARS)
 
-
 LOCAL_SRC_FILES := lights.c
 LOCAL_MODULE_RELATIVE_PATH := hw
 LOCAL_SHARED_LIBRARIES := liblog libcutils
 LOCAL_CFLAGS := -DLOG_TAG=\"qdlights\"
+LOCAL_CFLAGS += -DWHITE_LED
 LOCAL_CLANG  := true
 LOCAL_MODULE := lights.$(TARGET_BOARD_PLATFORM)
 LOCAL_HEADER_LIBRARIES := libhardware_headers
 LOCAL_MODULE_TAGS := optional
 LOCAL_VENDOR_MODULE := true
-ifneq (,$(filter tissot,$(TARGET_DEVICE)))
-    LOCAL_CFLAGS += -DWHITE_LED
-endif
 
 include $(BUILD_SHARED_LIBRARY)
