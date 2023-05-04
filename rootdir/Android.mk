c05310acba254701ba1020560dac3220f5a26bc3
dib12 <61652361+dib12@users.noreply.github.com>
2023-05-04 09:48:58 +0530

    msm8953-common: Drop 3.18 and AB support

--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -6,15 +6,10 @@ include $(CLEAR_VARS)
 LOCAL_MODULE       := fstab.qcom
 LOCAL_MODULE_TAGS  := optional
 LOCAL_MODULE_CLASS := ETC
-ifeq ($(AB_OTA_UPDATER), true)
-LOCAL_SRC_FILES    := fstab_AB.qcom
-else
 LOCAL_SRC_FILES    := fstab.qcom
-endif
 LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR_ETC)
 include $(BUILD_PREBUILT)
 
-ifeq ($(TARGET_KERNEL_VERSION),4.9)
 include $(CLEAR_VARS)
 LOCAL_MODULE	   := init.msm.usb.configfs.rc
 LOCAL_MODULE_TAGS  := optional
@@ -22,7 +17,6 @@ LOCAL_MODULE_CLASS := ETC
 LOCAL_SRC_FILES    := init.msm.usb.configfs.rc
 LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR_ETC)/init/hw
 include $(BUILD_PREBUILT)
-endif
 
 include $(CLEAR_VARS)
 LOCAL_MODULE       := init.qcom.rc
@@ -47,7 +41,6 @@ LOCAL_MODULE_CLASS := ETC
 LOCAL_SRC_FILES    := init.target.rc
 LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR_ETC)/init/hw
 include $(BUILD_PREBUILT)
-
 
 include $(CLEAR_VARS)
 LOCAL_MODULE       := init.qcom.usb.rc
