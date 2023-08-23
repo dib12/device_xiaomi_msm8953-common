15357ec7221a132fa44d4b0fa181cc4edfdca891
dib12 <61652361+dib12@users.noreply.github.com>
2023-08-23 21:33:15 +0530

    common: Update some radio overlay from vince

--- msm8953.mk
+++ msm8953.mk
@@ -335,10 +335,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     prebuilts/vndk/v29/arm64/arch-arm64-armv8-a/shared/vndk-core/libprotobuf-cpp-full.so:$(TARGET_COPY_OUT_VENDOR)/lib64/libprotobuf-cpp-full.so
 
-# Tethering
-PRODUCT_PACKAGES += \
-    TetheringConfigOverlay
-
 # Vibrator
 PRODUCT_PACKAGES += \
     vendor.qti.hardware.vibrator.service
