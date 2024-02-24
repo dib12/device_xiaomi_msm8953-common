a8a1c55314551f84b092d9dc68e7b0e6e8b96460
dib12 <61652361+dib12@users.noreply.github.com>
2024-02-24 07:13:40 +0530

    common: thermal: Adapt to msm8953

--- msm8953.mk
+++ msm8953.mk
@@ -319,6 +319,9 @@ PRODUCT_BOOT_JARS += \
     telephony-ext
 
 # Thermal
+PRODUCT_PACKAGES += \
+    android.hardware.thermal@2.0-service.qti-msm8953
+
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/thermal-engine.conf:$(TARGET_COPY_OUT_VENDOR)/etc/thermal-engine.conf
 
