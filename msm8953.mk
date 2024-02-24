0d6ae2b50d7528a042799e2de6a804ab5ef019f0
dib12 <61652361+dib12@users.noreply.github.com>
2024-02-24 20:03:08 +0530

    common: Build libprotobuf-cpp-lite-3.9.1-vendorcompat for libwvhidl.so

--- msm8953.mk
+++ msm8953.mk
@@ -337,6 +337,9 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     prebuilts/vndk/v29/arm64/arch-arm64-armv8-a/shared/vndk-core/libprotobuf-cpp-full.so:$(TARGET_COPY_OUT_VENDOR)/lib64/libprotobuf-cpp-full.so
 
+PRODUCT_PACKAGES += \
+    libprotobuf-cpp-lite-3.9.1-vendorcompat
+
 # Vibrator
 PRODUCT_PACKAGES += \
     vendor.qti.hardware.vibrator.service
